d463469487ada4114226c1ee7f42b1fbe0b5e979
zero-0002 <zero.dev0002@gmail.com>
2026-08-15 19:44:27 +0900

    remove and create new files.

--- common/darwin/Classes/LocalTrack.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@protocol LocalTrack <NSObject>
-
-- (RTCMediaStreamTrack*)track;
-
-@end
--- lib/src/video_renderer_extension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_webrtc/flutter_webrtc.dart';
-
-extension VideoRendererExtension on RTCVideoRenderer {
-  RTCVideoValue get videoValue => value;
-}
-
-abstract class AudioControl {
-  Future<void> setVolume(double volume);
-}
